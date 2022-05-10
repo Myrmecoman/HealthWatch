9932b500f684ceacd1c75789937618259ff81b54
Myrmecoman <45721327+Myrmecoman@users.noreply.github.com>
2022-05-11 00:29:41 +0200

    préparation pour affichage écran

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import machine
 import pins
 
 
-i2c = machine.SoftI2C(scl=screenScl, sda=screenSda)
+i2c = machine.SoftI2C(scl=pins.screenScl, sda=pins.screenSda)
 screen = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+screen.fill(1)
--- pins.py
+++ pins.py
@@ -1,5 +1,8 @@
-import machine
+from pyb import Pin
 
 
-screenScl = machine.Pin('B8')
-screenSda = machine.Pin('B9')
+screenSda = Pin('A8')
+screenScl = Pin('A9')
+screenVoltage = Pin('B7', Pin.OUT)
+screenVoltage.value(1)                          # send current
+screenGround = Pin('B6', Pin.IN, Pin.PULL_DOWN) # pull current
